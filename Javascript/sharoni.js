06d5d085684687e2667f51c428abaf7d5478ed3d
sharonishoshi <sikdersharoni6@gmail.com>
2024-03-12 09:57:02 +0600

    organized this folder

---
+++ Javascript/sharoni.js
@@ -0,0 +1,139 @@
+/*let i=2;
+do{
+    document.write(i+"<br>");
+    i=i+2;
+}while(i<22);*/
+
+/*let stinfo = ["Sharoni",21,"Magura"];
+for(let i=0;i<=2;i++){
+    document.write(stinfo[i]+"<br>");
+}*/
+
+/*let stinfo = {name:"Sharoni", age:21,address:"Magura"};
+document.write(stinfo.address+"<br>"+stinfo.age+"<br>"+stinfo.name);*/
+
+/*document.getElementById("ss").src ="SHARONI/wp.jpg";
+
+let ss =[20,2];
+for(let i= 0;i<=2;i=i+1){
+console.log(ss[0]*ss[1])
+}
+
+
+let a = [2,4,5,12,23];
+for (let i=0;i<=4;i++){
+    if(a[i]==0){
+document.write(" It is null")
+    }
+    else if(a[i]%2==0){
+   document.write (a[i]+"is an even number<br>");
+}
+else{
+    document.write(a[i]+"is an odd number<br>");
+}
+}*/
+
+
+/*let j=1;
+while(true){
+    if(j>10) break;
+    document.write(j);
+    j++;
+}*/
+
+
+/*function name (){
+    return "Sharoni";
+}
+document.write(name());
+
+function farenheit(celcius){
+    return celcius*(9/5)+32;
+}
+document.write(farenheit(20));
+
+function area(redius){
+    return 3.1416*redius**2
+}
+document.write(area(10));
+
+function average2(a,b){
+    return (a+b)/2;
+}
+function average3(a,b,c,){
+    return (a+b+c)/3;
+}
+document.write(average3(10,6,2));*/
+
+
+/*function tax(salary){
+    if(salary<=2000){
+        return "without tax";
+    }
+    else if(salary>2000 && salary<=3000){
+        return (salary*8)/100;
+    }
+
+    else if(salary>3000 && salary<=4500){
+        return(salary*18)/100;
+    }
+    else if(salary>4500){
+        return(salary*28)/100;
+    }
+}
+document.write("<br>"+tax(2500));*/
+
+    /*function day(year, month, day){
+    return(year*365+month*30+day);
+}
+document.write(day(2,3,15));*/
+
+
+    /*function time(hour, minute, second){
+    return(hour*60*60+minute*60+second);
+    }
+    document.write(time(3,10,12));*/
+
+
+   /* function Image1(){
+        document.getElementById('ss').src='SHARONI/s.jpg';
+    }
+
+    function Image2(){
+        document.getElementById('ss').src='SHARONI/wp.jpg';
+    }*/
+    
+    //  let a =["sharoni",8,"sikder"];
+    //  for (let i=0;i<3;i++){
+    //    document.write((a[i])+"<br>")
+    //  }
+
+    // let b={name:"sharoni",district:"Magura",year:"2024"};
+    // document.write(b.year);
+
+    // let array = [4,10,15,12];
+    // document.write(array[3]);
+    // for(let i=0; i<4; i++){
+    //     document.write((i<3)+"<br>")
+    // }
+
+    // <concadition section></concadition>
+    // let car={brand:"TATA",color:"Black",price:500000}
+    // let string ="This is "+car.brand+" car."+"<br>Its color is "+car.color+".<br>Its price is "+car.price+".";
+    // document.write(string);
+
+    // function traverse(array){
+    //     for (let i=0; i<array.length;i++){
+    //     document.write(array[i]+"<br>");
+    // }
+    //  }
+    //  let a =[5,6,10,3];
+    //  let b =[4,6,1,3];
+    //  let c =[1,2];
+    //  traverse(a,a.length);
+    //  traverse(b,b.length);
+    //  traverse(c,c.length);
+     
+
+
+
